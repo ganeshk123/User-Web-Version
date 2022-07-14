698a3ab9c5e85088dcccf3defb080d2d01df3352
ganeshk123 <ganesh.korde7@gmail.com>
2022-07-14 21:25:52 +0530

    Added my project

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,43 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Grocery Delivery App
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2021-present initappz.
+*/
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'Contactless Shopping'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Contactless Shopping');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Contactless Shopping app is running!');
+  });
+});
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,21 @@
+/*
+  Authors : Conactless Shopping
+  Website : https://csuk.uk/
+  App Name : Contactless Shopping App
+  This App Template Source code is licensed as per the
+  terms found in the Website https://csuk.uk/license
+  Copyright and Good Faith Purchasers © 2022-present Contactless Shopping.
+*/
+export const environment = {
+  production: true,
+  baseUrl: 'https://api.csuk.uk/public/api/', // ex https://csuk.uk/public/api/ don't forgot to add public/api/ at the end
+  imageUrl: 'https://api.csuk.uk/public/storage/images/', // ex https://csuk.uk/public/storage/images/ don't forgot to add public/storage/images at the end
+  firebase: {
+    apiKey: "YOURKEY",
+    authDomain: "YOURKEY",
+    projectId: "YOURKEY",
+    storageBucket: "YOURKEY",
+    messagingSenderId: "YOURKEY",
+    appId: "YOURKEY"
+  }
+};
